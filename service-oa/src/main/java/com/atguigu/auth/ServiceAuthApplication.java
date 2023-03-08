706af2ed88a9ca0eaaa574f4a5cdb5ea9d6b4292
Elroy-W <elroy7777777.com>
2023-03-08 16:56:28 +0800

    实现角色分页查询以及角色的增删改

--- service-oa/src/main/java/com/atguigu/auth/ServiceAuthApplication.java
+++ service-oa/src/main/java/com/atguigu/auth/ServiceAuthApplication.java
@@ -3,9 +3,10 @@ package com.atguigu.auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.atguigu.auth.mapper")
+@ComponentScan("com.atguigu")
 public class ServiceAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceAuthApplication.class, args);
